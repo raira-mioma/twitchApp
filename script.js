c1e956a8b72baa15de983f57e1ee1318e8ab812d
Raira <s.meniailova@gmail.com>
2017-09-25 12:59:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", 
+	"freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas",
+	"cherryberrymango2"];
+
+
 $(document).ready(function() {
    
-  $.support.cors = true;
-  getChannels();
-  
+  /*$.support.cors = true;*/
+  channels.forEach(function(channel) {
+	getChannel(channel);  
+  }
+  );
+
 });
 
-function getChannels() {
+function getChannel(name) {
 	
-	var url = "https://wind-bow.gomix.me/twitch-api/streams/RobotCaleb?callback=?";
+	var url = "https://wind-bow.gomix.me/twitch-api/streams/"+ name +"?callback=?";
 
 		$.ajax({
 		url: url,
@@ -17,14 +25,14 @@ function getChannels() {
 		dataType: "json",
 		success: function(data, status, jqXHR) {
 			
-			appendChannels(data);
+			appendChannels(data, name);
 		}
 	});
 
 		
 };
 
-function appendChannels(data) {
+function appendChannels(data, name) {
 	/*
 	for (var i=0; i<10; i++){
 		$("#all > ul").append("<li><a href=#>Channel " + i + "</a></li>");
@@ -35,12 +43,13 @@ function appendChannels(data) {
 	
 	if (data.stream!== null)
 	{
-		$("#online > ul").append("<li><a href=" + '"' + data._links.channel.value + '"' + ">" + data._links.channel.value + "</a></li>");
+		$("#online > ul").append("<li><a href=" + '"' + data.stream.channel.url + '"' + ">" + data.stream.channel.display_name  + "</a></li>");
+		$("#all > ul").append("<li><a href=" + '"' + data.stream.channel.url + '"' + ">" + data.stream.channel.display_name + "<span class='stream-details'>" + data.stream.channel.game +  "</span>" + "</a></li>");
 	}
 	else
 	{
-		$("#all > ul").append("<li><a href=" + '"' + data._links.channel.value + '"' + ">" + data._links.channel.value + "</a></li>");
-		$("#offline > ul").append("<li><a href=" + '"' + data._links.channel.value + '"' + ">" + data._links.channel.value + "</a></li>");
+		$("#all > ul").append("<li><a href=https://www.twitch.tv/" + name + ">" + name  + "<span class='stream-details'>Offline</span>" + "</a></li>");
+		$("#offline > ul").append("<li><a href=https://www.twitch.tv/" + name + ">" + name + "</a></li>");
 	}
 	
 };
